60e24291f774ef5a54eecb509197fa6678c4aa64
kaduell <92281981+Mystaneo@users.noreply.github.com>
2021-10-11 05:05:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# introduction
+print("Welcome to the Band Name Generator.")
+# question 1: city user grew up in
+question1 = input("What's name of the city you grew up in?\n")
+# question 2: name of user's pet
+question2 = input("What's your pet's name?\n")
+# combine both user questions
+print("Your band name could be " + question1 + " " + question2)
